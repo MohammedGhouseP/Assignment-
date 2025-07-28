d88a0ed5934b3c5c4de64bc7ea51d7176cf7ac64
MohammedGhouseP <ghousebashamail@gmail.com>
2025-07-28 16:08:54 +0530

    Simulating Asynchronous Data Fetching with Error Handling Using Async/Await

---
+++ promises/Q12.js
@@ -0,0 +1,22 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random(); 
+      if (random > 0.5) {
+        resolve("Fetched data successfully!");
+      } else {
+        reject("Something went wrong.");
+      }
+    }, 1000);
+  });
+}
+
+async function fetchDataHandler() {
+  try {
+    const result = await fetchData();
+    console.log(result);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
+fetchDataHandler();
